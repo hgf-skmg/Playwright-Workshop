8341ae234e02c10fb652edee4b3b09636ded7421
Felix Alleweldt <felix.alleweldt@hogrefe.de>
2025-04-04 14:57:05 +0200

    first task finished (login and logout)

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+/*
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   
@@ -18,4 +19,61 @@ test('get started link', async ({ page }) => {
   await page.waitForTimeout(3000)
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+*/
+
+test('open Prerelease, check for inputs and the login', async ({ page }) => {
+  
+
+  
+  await page.goto('https://eval.hogrefe-ws.com/HTSPrerelease/main#');
+
+  await page.waitForTimeout(1000);
+
+  const serialField = page.locator('input[name="login"]');
+  const passwordField = page.locator('input[name="password"]');
+  const submitButton = page.getByRole('button', { name: 'Log In' });
+
+  await expect(serialField).toBeVisible();
+  await expect(passwordField).toBeVisible();
+  await expect(submitButton).toBeVisible();
+
+  await serialField.fill("565203040a");
+  await passwordField.fill("DeJ2nYX31");
+  
+  await page.waitForTimeout(1000);
+
+  await submitButton.click();
+
+  await page.waitForTimeout(3000);
+
+  const displayText = page.locator('.display-warning');
+
+  await expect(displayText).toBeVisible();
+  
+  await displayText.evaluate((div) => {
+    div.style.backgroundColor = 'yellow'; // Hintergrundfarbe ändern
+  });
+
+  await page.waitForTimeout(2000);
+
+  await expect(displayText).toHaveText("Evaluation - Prerelease");
+
+  const splitButtonMenu = page.locator('#splitButtonMenu button');
+  //const splitButtonMenu = page.locator('[id^="splitButtonMenuDiv"]');
+
+  await expect(splitButtonMenu).toBeVisible();
+
+  await splitButtonMenu.click();
+
+  await page.waitForTimeout(1000);
+
+  const yesButton = page.locator('button', { hasText: 'Yes' });
+
+  await expect(yesButton).toBeVisible();
+
+  await page.waitForTimeout(1000);
+
+  await yesButton.click()
+
 });
